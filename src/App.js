cae0959bb233364a4cb3a1b180773e2a7f89bee0
karaevelize <k.evelize@gmail.com>
2019-12-13 16:34:02 +0100

    stream is working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { connect } from "react-redux";
 import "./App.css";
+import { allMessages } from "./actions";
 
 class App extends Component {
   stream = new EventSource("http://localhost:4000/stream");
   componentDidMount() {
     this.stream.onmessage = event => {
       console.log("event test:", event.data);
-      const parsed = JSON.parse(event.data);
-      console.log("parse test:", parsed);
+      const action = JSON.parse(event.data);
+      console.log("action test:", action);
+      this.props.dispatch(action);
     };
   }
   render() {
-    return <div>Client</div>;
+    console.log("this.props.message-test:", this.props);
+    const { messages } = this.props;
+    const list = messages.map(message => (
+      <p key={message.id}>{message.text}</p>
+    ));
+    return <div>{list}</div>;
   }
 }
 
-export default App;
+// get data from store
+
+function mapStateToProps(state) {
+  // state is the current data in the redux store
+  // each property of the object becomes a props of the component
+  return {
+    messages: state //inside the component this.props.messages will be the entire state state of the redux store
+  };
+}
+
+export default connect(mapStateToProps)(App);
