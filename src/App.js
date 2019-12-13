d832d0562ffe0a894c78b934371a3d7821083723
karaevelize <k.evelize@gmail.com>
2019-12-13 17:18:10 +0100

    form is ready and working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import superagent from "superagent";
 import "./App.css";
-import { allMessages } from "./actions";
 
 class App extends Component {
+  state = {
+    text: ""
+  };
+
   stream = new EventSource("http://localhost:4000/stream");
   componentDidMount() {
     this.stream.onmessage = event => {
@@ -13,13 +17,48 @@ class App extends Component {
       this.props.dispatch(action);
     };
   }
+
+  onChange = event => {
+    // const value = event.target.value
+    // const {value} = event.target
+    // const {target} = event
+    // const {value} = target
+    const {
+      target: { value }
+    } = event;
+
+    this.setState({ text: value });
+  };
+
+  onClick = () => {
+    this.setState({ text: "" });
+  };
+
+  onSubmit = async event => {
+    event.preventDefault();
+    const url = "http://localhost:4000/message";
+    const response = await superagent.post(url).send(this.state);
+    console.log("response test:", response);
+    this.onClick();
+  };
+
   render() {
     console.log("this.props.message-test:", this.props);
     const { messages } = this.props;
     const list = messages.map(message => (
       <p key={message.id}>{message.text}</p>
     ));
-    return <div>{list}</div>;
+    return (
+      <main>
+        <form onSubmit={this.onSubmit}>
+          <input onChange={this.onChange} type="text" value={this.state.text} />
+          <button>Submit</button>
+        </form>
+        <br />
+        <button onClick={this.onClick}>Reset Form</button>
+        {list}
+      </main>
+    );
   }
 }
 
